f6c05c079c8a6c172de43c19538670d5d7eb6976
KaZzy12 <xthel0@gmail.com>
2015-05-17 17:54:49 +0200

    Update Recherche.sql

--- Recherche.sql
+++ Recherche.sql
@@ -1,5 +1,5 @@
 CREATE or replace VIEW RechDateCmd
 		("Date", "Numero Commande", "Nom Fournisseur", "Nom Gestionnaire", "Etat")
-	as select c.Numero, f.Nom, u.Nom, c.ValidationCommande, c.Nom
+	as select c.Numero, f.Nom, u.Nom, c.ValidationCommande, c.ValidationManager
 	from Fournisseur f, CommandeFourn c, Utilisateur u
 	where f.Nom = c.NomFourn and u.Nom = c.NomUser and  c.DateCommande = (?);
