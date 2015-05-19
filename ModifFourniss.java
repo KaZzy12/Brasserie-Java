88c276e6528fae7482f19cbd884f321893a2dd58
KaZzy12 <xthel0@gmail.com>
2015-05-20 01:31:51 +0200

    Update ModifFourniss.java

--- ModifFourniss.java
+++ ModifFourniss.java
@@ -1,18 +1,18 @@
 //Insert new Fournisseur
 Connection connexion = … ;
 
-String instructionSQL = "INSERT INTO Fournisseur VALUES('"+getNom()+"', '"+getRue()+"', '"+getCodePostal()+"', '"+getLocalite()+"')";
+String instructionSQL = "INSERT INTO Fournisseur VALUES('"+fournisseur.getNom()+"', '"+fournisseur.getRue()+"', '"+fournisseur.getCodePostal()+"', '"+fournisseur.getLocalite()+"')";
 PreparedStatementprepStat = connexion.prepareStatement(instructionSQL);
 int nbIns = prepStat.executeUpdate();
 
 //Modif Fournisseur
 
-String instructionSQL = "UPDATE Fournisseur SET colonne = valeur where Nom = '"+getNom+"')"; 
+String instructionSQL = "UPDATE Fournisseur SET colonne = valeur where Nom = '"+fournisseur.getNom+"')"; 
 PreparedStatementprepStat = connexion.prepareStatement(instructionSQL);
 int nbIns = prepStat.executeUpdate( );
 
 //Supr Fournisseur
 
-String instructionSQL = "DELETE FROM Fournisseur where Nom = '"+getNom+"')"; 
+String instructionSQL = "DELETE FROM Fournisseur where Nom = '"+fournisseur.getNom+"')"; 
 PreparedStatementprepStat = connexion.prepareStatement(instructionSQL);
 int nbIns = prepStat.executeUpdate( );
