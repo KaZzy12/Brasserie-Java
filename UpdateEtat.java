8fb0332f84f7330c2ed5477bc324eec395e33d6f
KaZzy12 <xthel0@gmail.com>
2015-05-20 01:22:52 +0200

    Update UpdateEtat.java

--- UpdateEtat.java
+++ UpdateEtat.java
@@ -1,4 +1,4 @@
-String instructionSQL = "insert into CommandeFourn (Etat) values (?) (where Numero = (?))"; // where ??
+String instructionSQL = "insert into CommandeFourn (Etat) values (?) (where Numero = (?))"; // where ?? chap 9 slide 20 ?? 
 PreparedStatementprepStat= connexion.prepareStatement(instructionSQL);
 prepStat.setString(1, commande.getEtat());
 prepStat.executeUpdate( );
