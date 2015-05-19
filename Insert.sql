b357713a55f267fe1e33146fee90fb4615564a05
KaZzy12 <xthel0@gmail.com>
2015-05-19 15:05:29 +0200

    Create Insert.sql

---
+++ Insert.sql
@@ -0,0 +1,11 @@
+INSERT
+into Utilisateur
+VALUES ('MSeb','1','Lagnaule', 'Sebastian', 'Manager');
+
+INSERT
+into Utilisateur
+VALUES ('GLo','1','Cheron','Loic','Gestionnaire de stock');
+
+INSERT
+into Utilisateur
+VALUES ('CGhi','1','Muret','Ghislain','Comptable');
