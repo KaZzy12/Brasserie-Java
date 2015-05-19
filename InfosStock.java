dcdef5a23cf61feecb4b84b1589561a04e149c42
KaZzy12 <xthel0@gmail.com>
2015-05-19 23:46:17 +0200

    Create InfosStock.java

---
+++ InfosStock.java
@@ -0,0 +1,56 @@
+//infos stock
+
+// SELECT * FROM Produits where QteStock > 0;
+
+Resulset donnees = //resultat cmd sql
+
+Produit produit;
+int reference;
+String libelle;
+double prixBase;
+double prixVidange; 
+int TVA;
+double reduction; 
+java.sql.Date dateEd;
+double pourcReduc;
+int qteStock;
+
+ArrayList<Produit> allProduit = new ArrayList<Produit>();
+
+while (donnees.next()){
+	produit = new Produit (donnees.getInt("Reference"));
+	
+	produit.setLibelle(donnees.getString("Libelle"));
+	
+	produit.setPrixBase(donnees.getDouble("PrixBase"));
+	
+	prixVidange = donnees.getDouble("PrixVidange");
+	if (donnees.wasNull() == false){
+		produit.setPrixVidange(prixVidange);
+	}
+	
+	produit.setTVA(donnees.getInt("TVA"));
+	
+	reduction = donnees.getDouble("Reduction");
+	if (donnees.wasNull() == false){
+		produit.setReduction(reduction);
+	}
+	
+	dateEd = donnees.getDate("DateFinReduc");
+	if (donnees.wasNull() == false){
+		GregorianCalendar gc = new GregorianCalendar();
+		gc.setTime(dateEd);
+		produit.setDateEvenement(gc);
+	}
+	
+	pourcReduc = donnees.getDouble("PourcReduc");
+	if (donnees.wasNull() == false){
+		produit.setPourcReduc(pourcReduc);
+	}
+	
+	qteStock = donnees.getInt("QteStock");
+	if (donnees.wasNull() == false){
+		produit.setQteStock(qteStock);
+	}
+	
+}
