a8746b29416de464c45d89e73706d94604c67227
KaZzy12 <xthel0@gmail.com>
2015-05-20 08:58:41 +0200

    Update ModifsProduit.java

--- ModifsProduit.java
+++ ModifsProduit.java
@@ -1,18 +1,49 @@
 //Insert new produit
 Connection connexion = … ;
 
-String instructionSQL = "INSERT INTO Produit VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+String instructionSQL = "INSERT INTO Produit VALUES(?, ?, ?, NULL, ?)";
 PreparedStatementprepStat = connexion.prepareStatement(instructionSQL);
 prepStat.setInt(1,produit.getReference());
 prepStat.setString(2,produit.getLibelle());
 prepStat.setDouble(3,produit.getPrixBase());
-prepStat.setDouble(4,produit.getPrixVidange());
-prepStat.setInt(5,produit.getTVA());
-prepStat.setDouble(6,produit.getReduction());
-prepStat.setDate(7,new java.sql.Date(produit.getDateFinReduc().getTimeInMillis()));
-prepStat.setDouble(8,produit.getPourcReduc());
-prepStat.setInt(9,produit.getQteStock());
-int nbIns = prepStat.executeUpdate( );
+prepStat.setInt(4,produit.getTVA());
+prepStat.executeUpdate();
+
+
+if(produit.getPrixVidange() != NULL){
+	instructionSQL = "UPDATE Produit set PrixVidange = ? where Reference = "+produit.getReference();
+	PreparedStatementprepStat = connexion.prepareStatement(instructionSQL);
+	prepStat.setDouble(4,produit.getPrixVidange());
+	prepStat.executeUpdate();
+}
+
+if (produit.getReduction() != NULL){
+	instructionSQL = "UPDATE Produit set Reduction = ? where Reference = "+produit.getReference();
+	PreparedStatementprepStat = connexion.prepareStatement(instructionSQL);
+	prepStat.setDouble(6,produit.getReduction());
+	prepStat.executeUpdate();
+}
+
+if (produit.getDateFinReduc() != NULL){
+	instructionSQL = "UPDATE Produit set DateFinReduc = ? where Reference = "+produit.getReference();
+	PreparedStatementprepStat = connexion.prepareStatement(instructionSQL);
+	prepStat.setDate(7,new java.sql.Date(produit.getDateFinReduc().getTimeInMillis()));
+	prepStat.executeUpdate();
+}
+
+if (produit.getPourcReduc != NULL){
+	instructionSQL = "UPDATE Produit set PourcReduc = ? where Reference = "+produit.getReference();
+	PreparedStatementprepStat = connexion.prepareStatement(instructionSQL);
+	prepStat.setDouble(8,produit.getPourcReduc());
+	prepStat.executeUpdate();
+}
+
+if (produit.getQteStock != NULL){
+	instructionSQL = "UPDATE Produit set QteStock = ? where Reference = "+produit.getReference();
+	PreparedStatementprepStat = connexion.prepareStatement(instructionSQL);
+	prepStat.setInt(9,produit.getQteStock());
+	prepStat.executeUpdate();
+}
 
 //Modif produit
 
