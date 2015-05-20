465acd1497941fb554b2b936c082529dc5d02c23
KaZzy12 <xthel0@gmail.com>
2015-05-20 02:47:25 +0200

    Update CreationCommande.java

--- CreationCommande.java
+++ CreationCommande.java
@@ -1,14 +1,21 @@
 GregorianCalendar gregC = commande.getDateCommande();
 java.sql.Date sqlD = new java.sql.Date(gregC.getTimeInMillis());
 
-String instructionSQL ="INSERT INTO Commande VALUES ("+commande.getNumero()", "+sqlD+", '"+commande.getUtilisateur()"', '"+commande.getFournisseur()"', '"+commande.getEtat()+"')";
+String instructionSQL ="INSERT INTO Commande VALUES (?, "+sqlD+", ?, ?, ?)";
 PreparedStatement prepStat = connexion.preparedStatement(instructionSQL);
+prepStat.setInt(1,commande.getNumero());
+prepStat.setString(2,commande.getUtilisateur());
+prepStat.setString(3,commande.getFournisseur());
+prepStat.setString(4,commande.getEtat());
 prepStat.executeUpdate();
 
 for (i=0; i<allLignesProduits.size(); i++){
 	LigneProduit ligneProduit = new LigneProduit (allLignesProduits.get(i)); 
 	
-	String instructionSQL = "INSERT INTO LigneProduit VALUES ("+ligneProduit.getProduit()+", '"+ligneProduit.getCommandeFourn()+"', "+ligneProduit.Quantite()+")"; 
+	String instructionSQL = "INSERT INTO LigneProduit VALUES (?, ?, ?)"; 
 	PreparedStatement prepStat = connexion.preparedStatement(instructionSQL);
+	prepStat.setInt(1,ligneProduit.getProduit());
+	prepStat.setInt(2,ligneProduit.getCommandeFourn());
+	prepStat.setInt(3,ligneProduit.Quantite());
 	prepStat.executeUpdate();
 }
