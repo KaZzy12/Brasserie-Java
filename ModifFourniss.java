dcbc7a883448235bb172bcea1b42f2d3e2e684b8
KaZzy12 <xthel0@gmail.com>
2015-05-20 03:02:02 +0200

    Update ModifFourniss.java

--- ModifFourniss.java
+++ ModifFourniss.java
@@ -1,18 +1,24 @@
 //Insert new Fournisseur
 Connection connexion = … ;
 
-String instructionSQL = "INSERT INTO Fournisseur VALUES('"+fournisseur.getNom()+"', '"+fournisseur.getRue()+"', '"+fournisseur.getCodePostal()+"', '"+fournisseur.getLocalite()+"')";
+String instructionSQL = "INSERT INTO Fournisseur VALUES(?, ?, ?, ?)";
 PreparedStatementprepStat = connexion.prepareStatement(instructionSQL);
+prepStat.setString(1,fournisseur.getNom());
+prepStat.setString(2,fournisseur.getRue());
+prepStat.setInt(3,fournisseur.getCodePostal());
+prepStat.setString(4,fournisseur.getLocalite());
 int nbIns = prepStat.executeUpdate();
 
-//Modif Fournisseur
+//Modif Fournisseur A REVOIR !!!!!!!!!!!
 
-String instructionSQL = "UPDATE Fournisseur SET colonne = valeur where Nom = '"+fournisseur.getNom+"')"; 
+String instructionSQL = "UPDATE Fournisseur SET colonne = valeur where Nom = ?"; 
 PreparedStatementprepStat = connexion.prepareStatement(instructionSQL);
+prepStat.setString(1,fournisseur.getNom());
 int nbIns = prepStat.executeUpdate( );
 
 //Supr Fournisseur
 
-String instructionSQL = "DELETE FROM Fournisseur where Nom = '"+fournisseur.getNom+"')"; 
+String instructionSQL = "DELETE FROM Fournisseur where Nom = ?"; 
 PreparedStatementprepStat = connexion.prepareStatement(instructionSQL);
+prepStat.setString(1,fournisseur.getNom());
 int nbIns = prepStat.executeUpdate( );
