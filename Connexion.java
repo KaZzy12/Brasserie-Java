77a54de19b7866653db3655ae7c85e5f28b9ff6d
KaZzy12 <xthel0@gmail.com>
2015-05-21 05:18:45 +0200

    Create Connexion.java

---
+++ Connexion.java
@@ -0,0 +1,21 @@
+ public getConnect (Connect connect)
+ {
+    Connection connexion = Singleton.getInstance();
+    String instructionSQL = "select * from Utilisateur where login = ? and password = ?)";
+    preparedStatement prepStat = connexion.preparedStatement(instructionSQL);
+    prepStat.setString(1,connect.getLogin());
+    prepStat.setString(2,connect.getPassword());
+    ResultSet donnees = prepStat.executeQuery();
+    
+    if (donnees.wasNull() == false)
+    {
+      Utilisateur user = new Utilisateur (donnees.getString("Login"), donnees.getString("Password"), donnees.getString("Nom"), donnees.getString("Prenom"), donnees.getString("Fonction"),
+    }else
+     exeption
+ 
+
+
+
+
+ 
+ 
